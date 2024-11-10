2efbd2ff151d0a186032c350ddb0fc89bc08a17c
KIMJANGGYU <jangkuok@naver.com>
2024-11-10 17:03:59 +0900

    15차 commit - 주문 기능 수정

--- src/components/order/OrderComponent.js
+++ src/components/order/OrderComponent.js
@@ -92,10 +92,11 @@ function OrderComponent(props) {
         jquery.src = "https://code.jquery.com/jquery-1.12.4.min.js";
         const iamport = document.createElement("script");
         iamport.src = "https://cdn.iamport.kr/js/iamport.payment-1.1.7.js";
-        document.head.appendChild(jquery);
-        document.head.appendChild(iamport);
+        document.head.appendChild(jquery)
+        document.head.appendChild(iamport)
         return () => {
-            document.head.removeChild(jquery); document.head.removeChild(iamport);
+            document.head.removeChild(jquery)
+            document.head.removeChild(iamport)
         }
     }, []);
 
